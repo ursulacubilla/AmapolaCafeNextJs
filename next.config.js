01c9f25ef4f7fcd484f23248a71fd908f1581342
ursulacubilla <ursulacubilla@gmail.com>
2024-04-01 14:41:04 +0200

    Changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    distDir: 'build',
+  }
+   
+  module.exports = nextConfig
--- src/app/components/testService.js
+++
@@ -1,6 +0,0 @@
-'use server'
-export async function testService (prevState, formdata) {
-  console.log(formdata, 'this is the form Data')
-  return { message: 'correct response' }
-  // fetch de la data hacia el backend
-}
